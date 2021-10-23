24fe1bc20115822c45ae5dafd1a6e1f4f1b8002e
Luis Torres <luisdtor@gmail.com>
2021-10-23 15:49:38 -0500

    Implemented breadth-first search

--- custom-bst/testing.js
+++ custom-bst/testing.js
@@ -19,8 +19,5 @@ tree.insert(20);
 tree.insert(170);
 tree.insert(15);
 tree.insert(1);
-tree.insert(18);
 
-tree.remove(9);
-
-console.log(JSON.stringify(traverse(tree.root)));
+console.log(tree.breadthFirstSearch());
